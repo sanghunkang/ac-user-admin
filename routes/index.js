7cb969fd22097372b56d1f6d26b53e601e30f325
sanghunkang <sanghunkang.dev@gmail.com>
2019-11-16 20:33:23 +0900

    Revert "initial commit"
    This reverts commit de56c4418a762ec8c1c218926b8c1fe2d2083d71.

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.status(200).send('working');
-});
-
-module.exports = router;
